ad37463589f08bbad1e960016dba47863b4b8cf4
nicholasbrean91 <31775539+nicholasbrean91@users.noreply.github.com>
2018-12-14 14:08:04 -0500

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Stack {
+	private final ArrayList<Integer> list = new ArrayList<Integer>();
+	
+	public void push(int num) {
+		list.add(num);
+	}		
+	
+	public void print() {
+		System.out.print(list);
+	}
+}
