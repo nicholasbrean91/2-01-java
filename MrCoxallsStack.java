f396a2d85aaa14a7a778b50c9d62f65620fee18a
nicholasbrean91 <31775539+nicholasbrean91@users.noreply.github.com>
2018-12-14 14:07:45 -0500

    Create MrCoxallsStack.java

---
+++ MrCoxallsStack.java
@@ -0,0 +1,23 @@
+//Created By: Nicholas Brean
+//Created On: October 2018
+//Created to add a number to a stack
+
+import java.util.Scanner;
+public class MrCoxallsStack {
+	
+	public static void main(String args[]){
+	Scanner userInput = new Scanner(System.in);
+	Stack stackNum = new Stack();
+	
+		do {
+	System.out.println("Enter a number to be placed on the Stack");
+	int userNum = userInput.nextInt();
+	if(userNum > 0) {
+	stackNum.push(userNum);
+	stackNum.print();
+	} else if (userNum == -1) {
+		break;
+	}
+		}while(true);
+	}
+}
